6e2cc51bd4e35d505423df229c826e1bb1b9d069
Pawel Mendelski <mendelski.pawel@gmail.com>
2021-09-17 16:47:34 +0200

    Update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 // tslint:disable:no-console
-import bodyParser from 'body-parser';
 import express from 'express';
 import * as rawConfig from '../config.json';
 import { Config } from './Config';
@@ -10,15 +9,16 @@ const config = Config.from(rawConfig);
 const port = config.port;
 
 app.use(logger());
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.all('*', (req, res, _) => {
   console.log('>>> url:', req.url);
   console.log('>>> method:', req.method);
   console.log('>>> headers:', req.headers);
   console.log('>>> body:', req.body);
-  const delay = Number.parseInt(req.query.delay || '0', 10);
-  const status = Number.parseInt(req.query.status || '200', 10);
+  console.log('');
+  const delay = Number.parseInt('' + (req.query.delay || '0'), 10);
+  const status = Number.parseInt('' + (req.query.status || '200'), 10);
   const message = req.query.msg || 'Dummy message';
   const sendResponse = () => {
     res.set('X-Dummy', 'dummy');
